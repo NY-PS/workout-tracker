9627830e7124ad7a9476974cda9ac54ee88cf8e4
Pulak Saha <sahap@oregonstate.edu>
2019-05-09 19:43:07 -0400

    Update user.py

--- workout_tracker/forms/user.py
+++ workout_tracker/forms/user.py
@@ -1,14 +1,33 @@
-class RegistrationForm(FlaskForm):
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import (
+    DataRequired, Length, Email
+)
+
+from workout_tracker.database import Database
+
+
+def get_user_roles():
+    with Database() as conn:
+        query = """
+        SELECT row_number() over() row_number,
+               e.enumlabel user_role
+        FROM pg_type t
+          JOIN pg_enum e ON t.oid = e.enumtypid
+          JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
+        WHERE n.nspname = 'public' AND t.typname = 'role'
+        ORDER BY 1"""
+
+        return conn.query(query).all()
+
+
+class UserForm(FlaskForm):
     username = StringField(
         'Username', validators=[DataRequired(), Length(min=3, max=16)]
     )
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField(
-        'Confirm Password', validators=[DataRequired(), EqualTo('password')]
-    )
     fname = StringField('First Name', validators=[])
     lname = StringField('Last Name', validators=[])
-    email = StringField('Email Address', validators=[DataRequired(), Email()])
+    email = StringField('Email Address', validators=[Email()])
     user_role = SelectField(
         'Role', validators=[], choices=get_user_roles(), coerce=int,
         default=next(v[0] for v in get_user_roles() if v[1] == 'user')

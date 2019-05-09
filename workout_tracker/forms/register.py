8fa77e59ae43a9089d943d77d3e5e6ebc4e9d379
Pulak Saha <sahap@oregonstate.edu>
2019-05-09 19:43:31 -0400

    Update register.py

--- workout_tracker/forms/register.py
+++ workout_tracker/forms/register.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import (
     DataRequired, Length, EqualTo, ValidationError, Email
 )
@@ -15,15 +15,7 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField(
         'Confirm Password', validators=[DataRequired(), EqualTo('password')]
     )
-    fname = StringField('First Name', validators=[])
-    lname = StringField('Last Name', validators=[])
     email = StringField('Email Address', validators=[DataRequired(), Email()])
-    user_role = SelectField(
-        'Role', validators=[], choices=get_user_roles(), coerce=int,
-        default=next(v[0] for v in get_user_roles() if v[1] == 'user')
-    )
-    city = StringField('City')
-    state = StringField('State')
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
